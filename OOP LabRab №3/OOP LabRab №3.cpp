08b3a69e952911dd2cc1310757328bb4bf0ca5ff
Руслан Кашапов <k.ruslan.ufa@gmail.com>
2020-12-09 23:47:00 +0500

    Добавьте файлы проекта.

---
+++ OOP LabRab №3/OOP LabRab №3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+class Object
+{
+public:
+    Object()
+    {
+
+    };
+    ~Object()
+    {
+        cout << "~Object" << endl;
+    }
+public:
+    void show()
+    {
+
+    }
+};
+
+class Storage
+{
+protected:
+    int size;
+    Object **object;
+public:
+    Storage(int size)//создаём динамический массив в конструкторе 
+    {
+        cout << "Storage(int size)" << endl;
+        this->size = size;
+        object = new Object *[size];
+    }
+    ~Storage()
+    {
+        cout << "~Storage()" << endl;
+        delete[] object;
+    }
+};
+
+int main()
+{
+    Storage mas(10);
+
+    
+}
+
